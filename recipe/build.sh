f5756cd92b5a660b4d08ff0a04f78f4208e8ad26
memento <claude.rc@gmail.com>
2024-07-13 21:50:17 -0500

    (ref) libs cannot be separated (only 1 .pc and .cmake)

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,15 +3,13 @@
 set -ex
 
 build_dir="${SRC_DIR}"/build-release
-pre_install_dir="${SRC_DIR}"/pre-install
 test_release_dir="${SRC_DIR}"/test-release
 
 mkdir -p "${build_dir}"
-mkdir -p "${pre_install_dir}"
 cd "${SRC_DIR}"/build-release
   # Downloads and install toolbox as a static lib, make sure to remove it
   cmake -S "${SRC_DIR}" -B . \
-  -D CMAKE_INSTALL_PREFIX="${pre_install_dir}" \
+  -D CMAKE_INSTALL_PREFIX="${PREFIX}" \
   -D CMAKE_BUILD_TYPE=Release \
   -D bip3x_BUILD_SHARED_LIBS=ON \
   -D bip3x_BUILD_JNI_BINDINGS=ON \
@@ -24,19 +22,16 @@ cd "${SRC_DIR}"/build-release
 cd "${SRC_DIR}"
 
 # Post-install toolbox removal
-find "${pre_install_dir}" -name '*toolbox*' -print0 | while IFS= read -r -d '' file; do
+find "${PREFIX}" -name '*toolbox*' -print0 | while IFS= read -r -d '' file; do
   rm -rf "${file}"
 done
 
 # Prepare test area
 mkdir -p "${test_release_dir}"
 cp -r "${build_dir}"/bin "${test_release_dir}"
-cd "${pre_install_dir}"
+cd "${PREFIX}"
   find . -name '*[Gg][Tt]est*' -print0 | while IFS= read -r -d '' file; do
     tar cf - "${file}" | (cd "${test_release_dir}" && tar xf -)
     rm -rf "${file}"
   done
 cd "${SRC_DIR}"
-
-# Transfer pre-install to PREFIX
-(cd "${pre_install_dir}" && tar cf - ./* | (cd "${PREFIX}" && tar xvf -))
